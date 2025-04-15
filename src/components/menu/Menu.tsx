6eac9ec1a276185e5a883a76efdcf3561b78393c
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-04-15 14:27:07 +0300

    add menu block

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+import { menu } from "../../menu_sections";
+import "./menu.scss";
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="menu">
+      {menu.map((item) => {
+        return (
+          <div key={item.id} className="item">
+            <span className="title">{item.title}</span>
+            {item.listItems.map((menu_item) => (
+              <Link to={menu_item.url} className="listItem">
+                <img src={`/${menu_item.icon}`} alt={menu_item.title} />
+                <span className="listItemLitle">{menu_item.title}</span>
+              </Link>
+            ))}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
