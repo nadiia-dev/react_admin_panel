a66907527e15f92eb264d13237919bb080cbd7b8
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-04-15 12:47:07 +0300

    add main navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+
 function App() {
-  return <></>;
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Outlet />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
