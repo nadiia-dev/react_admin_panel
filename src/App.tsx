b0d5d3f0cb9f8e1e50470893f584035ab3e52492
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-04-15 13:00:59 +0300

    add global styles and components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import "./styles/global.scss";
 
 function App() {
   const Layout = () => {
     return (
       <div className="main">
-        <Outlet />
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
       </div>
     );
   };
